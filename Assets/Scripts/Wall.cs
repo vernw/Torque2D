5defe6241ed32851660c0c59782345b029909483
mdzimmer <matt.dan.zim@gmail.com>
2016-09-04 15:12:56 -0700

    wall propagation
    BUG: does not handle corners well
    BUG: does not account for moving walls

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,28 +6,87 @@ public class Wall : MonoBehaviour {
 
     public float defaultFade;
 
+    float fadeTime = 1f;
+    float width;
+    float propagateDelay = .1f;
+    int propagateDistance = 3;
+    Wall north;
+    Wall east;
+    Wall south;
+    Wall west;
+
     void Start()
     {
         DOTween.Init();
 
         defaultFade = gameObject.GetComponent<SpriteRenderer>().color.a;
+        width = GetComponent<SpriteRenderer>().bounds.extents.x * 2;
+		north = getWall(Vector2.up);
+		east = getWall(Vector2.right);
+		south = getWall(Vector2.down);
+		west = getWall(Vector2.left);
     }
 
     // Pulses when avatars collide with wall segments
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "P1" || coll.gameObject.tag == "P2" || coll.gameObject.tag == "P3" || coll.gameObject.tag == "P4")
-        {
-            StartCoroutine(Pulse());
+    void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.tag == "P1" || coll.gameObject.tag == "P2" || coll.gameObject.tag == "P3" || coll.gameObject.tag == "P4") {
+			StartCoroutine (Pulse ());
+			if (north) north.doPulse(DIR.NORTH, propagateDistance);
+			if (east) east.doPulse(DIR.EAST, propagateDistance);
+			if (south) south.doPulse(DIR.SOUTH, propagateDistance);
+			if (west) west.doPulse(DIR.WEST, propagateDistance);
         }
     }
 
+    Wall getWall (Vector2 dir)
+	{
+		RaycastHit2D[] hits = Physics2D.RaycastAll (transform.position, dir, width);
+		foreach (RaycastHit2D hit in hits) {
+			Wall hitWall = hit.collider.GetComponent<Wall> ();
+			if (hitWall && hitWall != this) {
+				return hitWall;
+			}
+		}
+		return null;
+	}
+
+	void doPulse (DIR dir, int propagate)
+	{
+		StartCoroutine (Pulse ());
+		if (propagate > 0)
+			StartCoroutine(DelayedPropagate(dir, propagate - 1));
+	}
+
+	IEnumerator DelayedPropagate (DIR dir, int propagate)
+	{
+		yield return new WaitForSeconds(propagateDelay);
+		switch (dir) {
+			case DIR.NORTH:
+				if (north) north.doPulse(dir, propagate);
+			break;
+			case DIR.EAST:
+				if (east) east.doPulse(dir, propagate);
+			break;
+			case DIR.SOUTH:
+				if (south) south.doPulse(dir, propagate);
+			break;
+			case DIR.WEST:
+				if (west) west.doPulse(dir, propagate);
+			break;
+		}
+	}
+
     IEnumerator Pulse()
     {
-//        print("fadeup");
-        GetComponent<SpriteRenderer>().DOFade(100, 1.0f).WaitForCompletion();
-        GetComponent<SpriteRenderer>().DOFade(defaultFade, 2.0f).WaitForCompletion();
-//        print("fadeback");
-        yield return null;
+        yield return GetComponent<SpriteRenderer>().DOFade(1, fadeTime).WaitForCompletion();
+        yield return GetComponent<SpriteRenderer>().DOFade(defaultFade, fadeTime).WaitForCompletion();
+    }
+
+    enum DIR {
+    	NORTH,
+    	EAST,
+    	SOUTH,
+    	WEST
     }
 }
