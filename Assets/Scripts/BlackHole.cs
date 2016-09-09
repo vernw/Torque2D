2b9c0f01399aa19d379a4ba8b0bb1bf1395808d8
vernw <vrnnwng@gmail.com>
2016-09-08 19:58:13 -0700

    Asset Organization + HazardsController

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHole : MonoBehaviour {
+
+    public float radius = 5.0F;
+    public float strength = 10.0F;
+    public float duration = 5f;
+
+    private Vector3 _vacuumPos;
+    private Collider2D[] _colliders;
+
+    void Start () {
+        _vacuumPos = transform.position;
+        Invoke("Destruct", duration);
+    }
+
+    /*** THIS IS BROKEN, BUT IT'S FUNNY TO WATCH ***/
+    void Update()
+    {
+        _colliders = Physics2D.OverlapCircleAll(_vacuumPos, radius);
+
+        foreach (Collider2D hit in _colliders)
+        {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+                rb.AddForce((hit.GetComponent<Collider2D>().transform.position - _vacuumPos) * -strength);
+        }
+    }
+
+    void Destruct()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/HazardsController.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HazardsController : MonoBehaviour
+{
+    public static HazardsController instance = null;
+
+    public GameObject blackHolePrefab;
+    public GameObject wormholePrefab;
+    public GameObject gravityFieldPrefab;
+    public GameObject depthChargePrefab;
+    public GameObject powerUpPrefab;
+    public GameObject meteorShowerPrefab;
+
+    /*** Event Toggles ***/
+    public bool blackHoleActive;
+    public bool wormholeActive;
+    public bool gravityFieldActive;
+    public bool depthChargeActive;
+    public bool powerUpActive;
+    public bool meteorShowerActive;
+
+    public float hazardBuffer;
+    public float maxX, minX;
+    public float maxY, minY;
+
+    void Awake()
+    {
+        // Ensures singleton status of HazardsController
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        hazardBuffer = 10f;
+        maxX = 9f;
+        minX = -9f;
+        maxY = 3f;
+        minY = -3f;
+
+        SpawnEvents();
+    }
+
+    void SpawnEvents()
+    {
+        if (blackHoleActive)
+        {
+            StartCoroutine(SpawnBH());
+        }
+        if (wormholeActive)
+        {
+            StartCoroutine(SpawnWH());
+        }
+        if (gravityFieldActive)
+        {
+            StartCoroutine(SpawnGF());
+        }
+        if (depthChargeActive)
+        {
+            StartCoroutine(SpawnDC());
+        }
+        if (powerUpActive)
+        {
+            StartCoroutine(SpawnPU());
+        }
+        if (meteorShowerActive)
+        {
+            StartCoroutine(SpawnMS());
+        }
+    }
+
+    // Black hole spawner
+    IEnumerator SpawnBH()
+    {
+        yield return new WaitForSeconds(hazardBuffer + Random.Range(3f, 16f));
+
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+        GameObject blackHole = Instantiate(blackHolePrefab, pos, Quaternion.identity) as GameObject;
+        StartCoroutine(SpawnBH());
+    }
+
+    // Wormhole spawner
+    IEnumerator SpawnWH()
+    {
+        yield return new WaitForSeconds(hazardBuffer + Random.Range(0f, 16f));
+
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+        GameObject wormhole = Instantiate(wormholePrefab, pos, Quaternion.identity) as GameObject;
+        StartCoroutine(SpawnWH());
+    }
+
+    // Gravity field spawner
+    IEnumerator SpawnGF()
+    {
+        yield return new WaitForSeconds(hazardBuffer + Random.Range(0f, 16f));
+
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+        GameObject gravityField = Instantiate(gravityFieldPrefab, pos, Quaternion.identity) as GameObject;
+        StartCoroutine(SpawnGF());
+    }
+
+    // Depth charge spawner
+    IEnumerator SpawnDC()
+    {
+        yield return new WaitForSeconds(hazardBuffer + Random.Range(0f, 16f));
+
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+        GameObject depthCharge = Instantiate(depthChargePrefab, pos, Quaternion.identity) as GameObject;
+        StartCoroutine(SpawnDC());
+    }
+
+    // Powerup spawner
+    IEnumerator SpawnPU()
+    {
+        yield return new WaitForSeconds(hazardBuffer + Random.Range(0f, 16f));
+
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+        GameObject powerUp = Instantiate(powerUpPrefab, pos, Quaternion.identity) as GameObject;
+        StartCoroutine(SpawnPU());
+    }
+
+    // Meteor spawner
+    IEnumerator SpawnMS()
+    {
+        yield return new WaitForSeconds(hazardBuffer + Random.Range(0f, 16f));
+
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+        GameObject meteorShower = Instantiate(meteorShowerPrefab, pos, Quaternion.identity) as GameObject;
+        StartCoroutine(SpawnMS());
+    }
+}
