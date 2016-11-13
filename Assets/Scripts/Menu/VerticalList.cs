8a4d6dc2bd6ed393e408e560abe966ebbb0db90b
mdzimmer <matt.dan.zim@gmail.com>
2016-11-13 11:53:23 -0800

    sprint 6 ui scripts and prefabs

---
+++ Assets/Scripts/Menu/VerticalList.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VerticalList : MonoBehaviour {
+	public List<string> elements;
+	public float verticalSpacing = 40f;
+
+	float wordHeight = 40f;
+
+	// Use this for initialization
+	void Start () {
+		if (elements == null) {
+			elements = new List<string> ();
+		}
+		float curVerticalSpacing = verticalSpacing * elements.Count / 2 - wordHeight / 2f;
+		foreach (string element in elements) {
+			GameObject elementGO = Instantiate (Resources.Load("Prefabs/VerticalListElement", typeof(GameObject))) as GameObject;
+			elementGO.transform.position = new Vector2 (0, curVerticalSpacing);
+			elementGO.transform.parent = transform;
+			elementGO.GetComponent<VerticalListElement>().Initialize (element);
+			curVerticalSpacing -= verticalSpacing;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
