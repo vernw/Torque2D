a0e2be5126a8a6e39973712300e5246b9b085da9
mdzimmer <matt.dan.zim@gmail.com>
2017-01-15 14:40:13 -0800

    parallax background scaling with camera zoom

--- Assets/Scripts/Camera/parallax.cs
+++ Assets/Scripts/Camera/parallax.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class Parallax : MonoBehaviour {
 	float speed = .05f;
+//	float startWidth;
+	float cameraStartWidth;
 	MeshRenderer mr;
 	Vector3 cameraStart;
+	Vector3 startScale;
 
 	// Use this for initialization
 	void Start () {
 		mr = GetComponent<MeshRenderer> ();
 		cameraStart = Camera.main.transform.position;
+//		startWidth = transform.localScale.x;
+		startScale = transform.localScale;
+		cameraStartWidth = Camera.main.orthographicSize;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector2 offset = Camera.main.transform.position - cameraStart;
 		mr.material.mainTextureOffset = offset * speed;
+		float cameraWidth = Camera.main.orthographicSize;
+//		print (cameraWidth);
+		float ratio = cameraWidth / cameraStartWidth;
+		transform.localScale = startScale * ratio;
 	}
 }
